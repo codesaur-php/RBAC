c25928b41ff0bcb9cbecd760f53848bf26732885
Наранхүү Н <codesaur@gmail.com>
2021-10-22 17:03:00 +0800

    Update Accounts.php

--- src/Accounts.php
+++ src/Accounts.php
@@ -44,14 +44,13 @@ class Accounts extends Model
     {
         parent::__initial();
         
-        $table = $this->getName();
+        $table = $this->getName();        
         
-        $this->setForeignKeyChecks(false);
-        $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_created_by FOREIGN KEY (created_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
-        $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_updated_by FOREIGN KEY (updated_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
-        $this->setForeignKeyChecks(true);
-        
-        if ($table !== 'rbac_accounts') {
+        if ($table != 'rbac_accounts') {
+            $this->setForeignKeyChecks(false);
+            $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_created_by FOREIGN KEY (created_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+            $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_updated_by FOREIGN KEY (updated_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+            $this->setForeignKeyChecks(true);
             return;
         }
         
