b7b6fb170df7ac849836f7877aef3804b92e5e31
Наранхүү Н <codesaur@gmail.com>
2021-10-25 12:10:05 +0800

    Update UserRole.php

--- src/UserRole.php
+++ src/UserRole.php
@@ -45,8 +45,7 @@ class UserRole extends Model
         }
         
         $nowdate = date('Y-m-d H:i:s');
-        $query = "INSERT INTO $table(id,created_at,user_id,role_id)"
-                . " VALUES(1,'$nowdate',1,1)";
+        $query = "INSERT INTO $table(id,created_at,user_id,role_id) VALUES(1,'$nowdate',1,1)";
         $this->exec($query);
     }
 }
